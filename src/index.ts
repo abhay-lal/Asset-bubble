e7c8f761ebe5c305cb3fad8f35a450d21f6d707e
sreyom31 <sreyom31@gmail.com>
2022-09-10 10:49:49 +0530

    index file and ignore files added
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Welcome to Node typescript express boilerplate');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', async (req, res) => {
+  res.json({ status: 'Online' });
+});
+
+const port = Number(process.env.PORT ?? 5000);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
