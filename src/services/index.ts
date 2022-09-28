c216cb83fe19c90aed2d77ef5905de22978363f8
sreyom31 <sreyom31@gmail.com>
2022-09-28 12:45:54 +0530

    misc
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,15 +1,5 @@
 import userService from './user.service';
 import tokenService from './token.service';
 import authService from './auth.service';
-import paperService from './paper.service';
-import dateService from './date.service';
-import speakerService from './speaker.service';
 
-export {
-  userService,
-  tokenService,
-  authService,
-  paperService,
-  speakerService,
-  dateService,
-};
+export { userService, tokenService, authService };
--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,13 +1,4 @@
 import userValidation from './user.validation';
 import authValidation from './auth.validation';
-import speakerValidation from './speaker.validation';
-import paperValidation from './paper.validation';
-import dateValidation from './date.validation';
 
-export {
-  userValidation,
-  authValidation,
-  speakerValidation,
-  paperValidation,
-  dateValidation,
-};
+export { userValidation, authValidation };
