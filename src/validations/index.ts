ca059578e0dd06b14f82d96c900a0599904e1621
sreyom31 <sreyom31@gmail.com>
2022-09-28 09:12:29 +0530

    auth user validations
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

---
+++ src/validations/index.ts
@@ -0,0 +1,13 @@
+import userValidation from './user.validation';
+import authValidation from './auth.validation';
+import speakerValidation from './speaker.validation';
+import paperValidation from './paper.validation';
+import dateValidation from './date.validation';
+
+export {
+  userValidation,
+  authValidation,
+  speakerValidation,
+  paperValidation,
+  dateValidation,
+};
