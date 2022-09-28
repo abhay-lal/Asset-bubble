4ac5df56c05cc4c1ef2391f70715f7de5041f645
sreyom31 <sreyom31@gmail.com>
2022-09-28 12:44:39 +0530

    misc
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express';
 import authRoute from './auth.route';
 import userRoute from './user.route';
-import paperRoute from './paper.route';
-import speakerRoute from './speaker.route';
-import dateRoute from './date.route';
 
 const router = Router();
 
 const defaultRoutes = [
   { path: '/auth', route: authRoute },
   { path: '/users', route: userRoute },
-  { path: '/papers', route: paperRoute },
-  { path: '/speakers', route: speakerRoute },
-  { path: '/dates', route: dateRoute },
 ];
 
 defaultRoutes.forEach((route) => {
